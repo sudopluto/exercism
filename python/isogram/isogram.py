3c5892beaf4711c1631f3e62d1465e4bdcedf0aa
Pranav Sharma <pranav.sharma.ama@gmail.com>
2018-04-02 14:28:50 -0400

    did two more

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,11 @@
 def is_isogram(string):
-    pass
+    return is_isogram_help(string.lower())
+        
+def is_isogram_help(string):
+    if len(string) <= 1:
+        return True
+    elif not (string[0].isalpha()):
+        return is_isogram_help(string[1:])
+    else:
+        print(string[0], string[1:])
+        return (not (string[0] in string[1:])) and is_isogram_help(string[1:])
