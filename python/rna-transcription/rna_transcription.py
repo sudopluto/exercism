cc17bea5f59d931a2ce2ccc479f8a1b26ef6be2f
Pranav Sharma <pranav.sharma.ama@gmail.com>
2018-05-21 18:48:02 -0400

    did a couple of exercises

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,2 +1,8 @@
 def to_rna(dna_strand):
-    pass
+    temp = ''
+    dna_rna_dict = {'G' : 'C', 'C' : 'G', 'T' : 'A', 'A' : 'U'}
+    for base in dna_strand:
+        temp += dna_rna_dict[base]
+    return temp
+        
+
