b43bac33fa8cb72e25ce29b63174955838b6342d
Pranav Sharma <pranav.sharma.ama@gmail.com>
2018-05-22 13:53:00 -0400

    did about three more problems

---
+++ python/gigasecond/gigasecond.py
@@ -0,0 +1,5 @@
+import datetime
+
+def add_gigasecond(birth_date):
+
+    return birth_date + datetime.timedelta(seconds=10**9)
--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,2 +1,15 @@
+import re
+
 def word_count(phrase):
-    pass
+    word_hist = {}
+    phrase = phrase.lower()
+
+    phrase_as_list = re.split(r'[^a-zA-Z0-9\'"]', phrase)
+
+    phrase_as_list = list(filter(None, phrase_as_list))
+    phrase_as_list = list(map((lambda x: x.strip('\'"')), phrase_as_list))
+
+    for word in phrase_as_list:
+        word_hist[word]= word_hist.get(word, 0) + 1
+
+    return word_hist
