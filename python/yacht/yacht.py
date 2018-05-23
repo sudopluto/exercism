ed8944612e60276b3230dec8ce5af935bc40a9fd
Pranav Sharma <pranav.sharma.ama@gmail.com>
2018-05-23 14:06:15 -0400

    meetup is pretty scary: eek

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -1,18 +1,34 @@
 # Score categories
 # Change the values as you see fit
-YACHT = None
-ONES = None
-TWOS = None
-THREES = None
-FOURS = None
-FIVES = None
-SIXES = None
-FULL_HOUSE = None
-FOUR_OF_A_KIND = None
-LITTLE_STRAIGHT = None
-BIG_STRAIGHT = None
-CHOICE = None
+
+from functools import reduce
+
+YACHT = (lambda l: 50 if len(set(l)) == 1 else 0)
+ONES = (lambda l: len(list(filter((lambda e: e == 1), l))))
+TWOS = (lambda l: len(list(filter((lambda e: e == 2), l))) * 2)
+THREES = (lambda l: len(list(filter((lambda e: e == 3), l))) * 3)
+FOURS = (lambda l: len(list(filter((lambda e: e == 4), l))) * 4)
+FIVES = (lambda l: len(list(filter((lambda e: e == 5), l))) * 5)
+SIXES = (lambda l: len(list(filter((lambda e: e == 6), l))) * 6)
+FULL_HOUSE = (lambda l: CHOICE(l) if len(set(l)) == 2 
+                                    and (l.count(l[0]) == 3
+                                        or l.count(l[0]) == 2)
+                        else 0)
+FOUR_OF_A_KIND = (lambda l: 4 * sorted(l)[1]  if len(set(l)) <= 2 
+                                    and (l.count(l[0]) >= 4
+                                        or l.count([0]) <= 1)
+                        else 0)
+LITTLE_STRAIGHT = (lambda l: 30 if len(set(l)) == 5 
+                                    and (sorted(l)[0] == 1 
+                                        and sorted(l)[-1] == 5)
+                        else 0)
+BIG_STRAIGHT = (lambda l: 30 if len(set(l)) == 5 
+                                    and (sorted(l)[0] == 2 
+                                        and sorted(l)[-1] == 6)
+                        else 0)
+CHOICE = (lambda l: reduce((lambda e, a: a + e), l))
 
 
 def score(dice, category):
-    pass
+    return category(dice)
+
