68eb15184a9461253188baec685d0ca96e3ba9de
Pranav Sharma <pranav.sharma.ama@gmail.com>
2019-01-31 00:30:45 -0500

    started cpp track exercism

---
+++ cpp/leap/leap.cpp
@@ -0,0 +1,20 @@
+#include "leap.h"
+
+namespace leap {
+
+bool is_leap_year(int year) {
+  if (year % 4 == 0) {
+    if (year % 100 == 0 && year % 400 != 0) {
+      return false; 
+    } 
+    else {
+      return true; 
+    }
+  }
+  else {
+    return false; 
+  }
+
+}
+
+}
---
+++ cpp/leap/leap.h
@@ -0,0 +1,8 @@
+#ifndef LEAP
+#define LEAP
+namespace leap {
+
+bool is_leap_year(int year);
+
+}
+#endif
